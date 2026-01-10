99e022e8b05f2ba5558ab45231732aaf56f7c67d
33MarGomez <144274943+33MarGomez@users.noreply.github.com>
2026-01-10 18:48:14 -0500

    Update daubechies_probability_weighed_vectors.py
    Added fourier skimmer function. Updated example to correct dimensionality. Added function description to the db4fouriererrorvector function

--- daubechies_probability_weighed_vectors.py
+++ daubechies_probability_weighed_vectors.py
@@ -2,8 +2,9 @@ import numpy as np
 from scipy import stats as stat
 import pywt
 
-#this function returns the probability under normal curve as a weighed vector of the daubechies wave filter coefficients
 def db4errorvector(wavepacket,probability):
+  """ This divides by the number of std expected to have made up the geometric factor scaling coefficients to fourier mapping.
+  """
   z_score = stat.norm.ppf(probability,loc = 0, scale = 1)
   scalar = (np.float64(1.0) + np.sqrt(np.float64(3.0)))/(np.float64(4.0)*(np.sqrt(np.float64(2.0)))) / z_score
   unscaledvector = pywt.dwt(data = wavepacket,wavelet = 'db4')
@@ -12,11 +13,65 @@ def db4errorvector(wavepacket,probability):
 
   return scalar, scaledvector
 
-signal = [0,0.5,0.9,0.11,0,0,0.03,0.2,0.1] #sample signal vector
+def db4fourierskimmer(signal,probability):
+  """
+  This takes the confidence of qft adherence off the top and leaves behind deviant behaviour.
+  Add them back together for the original, fourier-amplified signal.
+  If d scales the fourth entry to the expected coefficient, and z is number of standard deviations causing the signal, and q is fourier conforming
+  dz*(signal)-q / (dz) = signal - (1/dz)*q
+
+  variables:
+  signal = signal vector
+  probability = 0 < p < 1 of observing the signal. Used for n_std.
+
+  returns:
+  flowpast = filter coefficients for the remainer
+  delta = surviving vector after losing fourier components
+
+  """
+  z_score = stat.norm.ppf(probability,loc = 0, scale = 1)
+  scalar =  (1.0 + np.sqrt(3.0))/(4.0*(np.sqrt(2.0))) / signal[3]
+  element = np.array([[(1.0 - np.sqrt(3.0))/(4.0 * np.sqrt(2.0))],
+                          [(3.0 - np.sqrt(3.0))/(4.0* np.sqrt(2.0))],
+                          [(3.0 + np.sqrt(3.0))/(4.0* np.sqrt(2.0))],
+                          [(1.0 + np.sqrt(3.0))/(4.0*(np.sqrt(2.0)))]
+                          ]
+                         )
+  dim = np.size(signal)
+  subtraction = np.zeros(shape=(dim,dim))
+  z = 0
+  while z < dim-2:
+    subtraction[z,z] = element[0].item()
+    subtraction[z+1,z+3] = -1.0 * element[0].item()
+    subtraction[z,z+2] = element[2].item()
+    subtraction[z+1,z+1] = -1.0 * element[2].item()
+    subtraction[z,z+3] = element[3].item()
+    subtraction[z+1,z] = element[3].item()
+    subtraction[z,z+1] = element[1].item()
+    subtraction[z+1,z+2] = element[1].item()
+    z += 2
+  subtraction[-2,-2] = element[0].item()
+  subtraction[-1,1] = -1.0 * element[0].item()
+  subtraction[-2,0] = element[2].item()
+  subtraction[-1,-1] = -1.0 * element[2].item()
+  subtraction[-2,1] = element[3].item()
+  subtraction[-1,-2] = element[3].item()
+  subtraction[-1,0] = element[1].item()
+  subtraction[-2,-1] = element[1].item()
+
+  delta = signal -  np.matmul(subtraction,signal) * ((z_score*scalar)**(-1.0))
+  flowpast = pywt.dwt(data = delta,wavelet = 'db4')
+
+  return flowpast, delta
+
+signal = [0.0,0.5,0.9,0.11,0,0,0.03,0.2] #sample signal vector
 p = 0.41 #sample probability
-scalar,vector = db4errorvector(signal,p)
+scalar,vector = db4fouriererrorvector(signal,p)
+coef,propagator = db4fourierskimmer(signal,p)
 print(str(scalar))
 print(str(vector))
+print('Modulus Filter coefficients = '+ str(coef))
+print('Modulus = ' + str(propagator))
 
 #generates wavelet filter coefficients for db4 in fourier mapping
 c_0 = 0
