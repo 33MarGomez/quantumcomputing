261b3881e0a8a9df8c1511eb17b2db936a96f807
33MarGomez <144274943+33MarGomez@users.noreply.github.com>
2026-01-11 13:08:23 -0500

    Update daubechies_probability_weighed_vectors.py
    Added a constructor for coefficients en lieu of scratch work to note -take. Corrects a mistake across published sources.

--- daubechies_probability_weighed_vectors.py
+++ daubechies_probability_weighed_vectors.py
@@ -3,7 +3,8 @@ from scipy import stats as stat
 import pywt
 
 def db4errorvector(wavepacket,probability):
-  """ This divides by the number of std expected to have made up the geometric factor scaling coefficients to fourier mapping.
+  """ 
+  This divides by the number of std expected to have made up the geometric factor scaling coefficients to fourier mapping.
   """
   z_score = stat.norm.ppf(probability,loc = 0, scale = 1)
   scalar = (np.float64(1.0) + np.sqrt(np.float64(3.0)))/(np.float64(4.0)*(np.sqrt(np.float64(2.0)))) / z_score
@@ -25,7 +26,7 @@ def db4fourierskimmer(signal,probability):
   probability = 0 < p < 1 of observing the signal. Used for n_std.
 
   returns:
-  flowpast = filter coefficients for the remainer
+  flowpast = filter coefficients for the modulus
   delta = surviving vector after losing fourier components
 
   """
@@ -64,6 +65,30 @@ def db4fourierskimmer(signal,probability):
 
   return flowpast, delta
 
+def coefficientconstructor(c_3):
+  """
+  Here is the derivation for the fourier wavelets in db4. On Wikipedia, and across 2 different books, the stated solution does not respect the symmetry
+  of the fourier transform but does satisfy all the requirements because it is only incorrect when substituted at an intermediate step:
+
+  (2*sqrt(3) -3)(1-sqrt(3) = 5 sqrt(3) - 15 NOT 3 + sqrt(3). It does work if you flip all the signs on the irrational, c_3 = 1 + sqrt(3) & c_1 = 3 - sqrt(3)
+
+  to prove one of the moments is wrong. Here is the corrected coefficient order, despite being cyclical, order does matter here.
+  This is just an efficient note-taking technique, this function essentially does nothing. The derivation works based on =0 not changing lin. comb.
+  """
+  #c_2 = c_0 * c_2 + c_1 * c_3 - c_2 * (c_0 -1.0 * c_1 + c_2 - c_3) + c_2 * (-1.0 * c_1 + 2* c_2 - 3 * c_3) + c_3 * (-1.0 * c_1 + 2 * c_2 - 3 * c_3)
+  c_2 = np.sqrt(3.0)j * c_3
+  c_1 = (2.0 * np.sqrt(3.0) - 3.0) * c_3
+  c_0 = np.sqrt(1.0 - c_1**2 - c_2**2 - c_3**2)
+  if c_0 - c_1 + c_2 - c_3 == 0:
+    print('Zeroeth moment is true')
+    return c_2
+  if 2.0 * c_2 - c_1 - 3.0 * c_3 == 0:
+    print('First moment is true')
+    return c_1
+  if c_0 == -1.0 * c_1 * c_3 / c_2:
+    print('Inverse matrix is transposition')
+    return c_0
+
 signal = [0.0,0.5,0.9,0.11,0,0,0.03,0.2] #sample signal vector
 p = 0.41 #sample probability
 scalar,vector = db4fouriererrorvector(signal,p)
@@ -72,51 +97,3 @@ print(str(scalar))
 print(str(vector))
 print('Modulus Filter coefficients = '+ str(coef))
 print('Modulus = ' + str(propagator))
-
-#generates wavelet filter coefficients for db4 in fourier mapping
-c_0 = 0
-c_1 = 0
-c_2 = 0.0
-c_3 = (np.float64(1.0) + np.sqrt(np.float64(3.0)))/(np.float64(4.0)*(np.sqrt(np.float64(2.0))))
-#occupy in reverse order
-
-known_amplitudes = 1 #how many coefficients do you already know the answer to.
-
-c_0 = np.float64(c_0)
-c_1 = np.float64(c_1)
-c_2 = np.float64(c_2)
-c_3 = np.float64(c_3)
-
-if known_amplitudes == 0:
-  print("Standard solution. This one's free chump!")
-  c_0 = (np.float64(1.0) - np.sqrt(np.float64(3.0)))/(np.float64(4.0)* np.sqrt(np.float64(2.0)))
-  c_1 = (np.float64(3.0) - np.sqrt(np.float64(3.0)))/(np.float(4.0)* np.sqrt(np.float64(2.0)))
-  c_2 = (np.float64(3.0) + np.sqrt(np.float64(3.0)))/(np.float64(4.0)* np.sqrt(np.float64(2.0)))
-  c_3 = (np.float64(1.0) + np.sqrt(np.float64(3.0)))/(np.float64(4.0)*(np.sqrt(np.float64(2.0))))
-  finalkey = np.array([c_0, c_1, c_2, c_3])
-  print(str(finalkey))
-
-if known_amplitudes == 3:
-  if c_0 == np.float64(0.0):
-    answer = np.float64(-1.0)* c_1 * c_3 /(c_2)
-    finalkey = np.array([answer, c_1, c_2, c_3])
-    print(str(finalkey))
-
-if known_amplitudes == 2:
-  c_1 = np.float64(2.0) * c_2 - np.float64(3.0) * c_3
-  c_0 = c_1 - c_2 + c_3
-  finalkey = np.array([c_0, c_1, c_2, c_3])
-
-if known_amplitudes == 1:
-  c_2 = np.sqrt(np.float64(3.0)) * c_3
-  c_1 = (np.float64(2.0) * np.sqrt(np.float64(3.0)) - np.float64(3.0)) * c_3
-  c_0 = c_1 - c_2 + c_3
-  N = np.sqrt(np.float64(1.0)/((c_0**2) + (c_1**2) + (c_2**2) + (c_3**2)))
-  c_0 = c_0 / N
-  c_1 = c_1 / N
-  c_2 = c_2 / N
-  c_3 = c_3 / N
-  finalkey = np.array([c_0, c_1, c_2, c_3])
-  print(finalkey)
-  check = finalkey[0] **2 + finalkey[3]**2 + finalkey[1]**2 + finalkey[2]**2
-  print(str(check) + ' should be =1')
