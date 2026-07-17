9669e8d9790b62f8f93527928c5a966de35b5ab2
33MarGomez <144274943+33MarGomez@users.noreply.github.com>
2026-07-17 08:23:28 -0400

    Update qiskit_collects_basic_expt.py
    Added pennylane circuit visualization for manual user-checks on the outcome. Added AER counts return if a user wants to visualize trials without using compute. Added spin echo J-coupling inner-product experiment.

--- qiskit_collects_basic_expt.py
+++ qiskit_collects_basic_expt.py
@@ -12,6 +12,7 @@ electronics and represents no dual-use cases.
 -Marco
 """
 !pip install 'qiskit[visualization]' qiskit-ibm-runtime qiskit-aer
+!pip install pennylane --upgrade
 
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
@@ -32,17 +33,141 @@ print("qiskit-aer:", qiskit_aer.__version__)
 print("qiskit-ibm-runtime:", qiskit_ibm_runtime.__version__)
 
 import pandas as pd
+import numpy as np
+import pennylane as qpl
 
 #run in a cell if in colab and delete to prevent accidental key disclosure
 QiskitRuntimeService.save_account(
     channel="ibm_quantum_platform",
     token="YOUR_TOKEN_HERE",
-    instance="YOUR_CDR"
+    instance="YOUR_CDR",
     overwrite=True,
     set_as_default=True,
 )
 
 #script
+three = qpl.device('default.qubit', wires=3)
+
+@qpl.qnode(three)
+def fabric_spin_echo():
+    qpl.RX(phi=np.pi*0.5,wires=0)
+    qpl.IsingZZ(phi=np.pi*0.5,wires=[0,1])
+    qpl.IsingYY(phi=np.pi*-0.5,wires=[0,1])
+    qpl.RY(phi=np.pi*2.0,wires=0)
+    qpl.IsingZZ(phi=np.pi*0.5,wires=[1,2])
+    qpl.IsingYY(phi=np.pi*-0.5,wires=[1,2])
+    qpl.IsingXX(phi=np.pi,wires=[0,1])
+    qpl.IsingXX(phi=np.pi,wires=[1,2])
+    return qpl.state(),qpl.probs(wires=[0,1,2])
+
+def check_sechos_expt():
+    """
+    Verifies aspects of the inner product with (s)pin (echos) applied to
+    prevent qubit evolution during collection.
+    
+    returns
+    Bool - Whether sechos has the correct properties of:
+         |101> & |111> are conj. amplitudes of same magnitude
+         |000> & |010> are real, same-magnitude amplitudes
+         |001> and |011> are the same imaginary amplitude
+         |100> and |110> are the same real amplitude
+         all probabilities are equal
+    
+    Theory- Exchanging upper 1 and 0 amplitudes exchanges complex and real
+    axis anti-symmetry on upper and lower registers. Because register
+    position cannot be exchanged, and complex phasing is a laboratory-frame
+    construct, no outstanding degree of freedom exists.
+    In the first (np.pi/4.0) ZZ and RX application, cos |0> takes on the same
+    value, the only such solution to not changing qubit amplitudes. As RX
+    (np.pi/2.0) is -i|1> and this projection is shared with RZ|1>, applying
+    YY towards -1|1> establishes chirality. The shared -1|1> Z cosine, -i|1>
+    RZ sine, RY cos |0> establish this. A spin echo is then applied as it
+    commutes with YY. Updates to the next qubit are expected to not impact
+    evolution as any interference in the qubit should evolve it to its starting
+    state in x-freedom. Therefore, XX should appear as the final ingredient in
+    the inner product.
+    """
+    vector,squared = fabric_spin_echo()
+    if (round(np.imag(vector[5]),4)
+        == -1.0*round(np.imag(vector[7]),4)):
+        pass
+    else:
+        print("|101> and |111> are not complex conjugate amplitudes\n"+
+              "as expected. Pass to API aborted.\nPlease check your gates"
+              +"\nas an undue property was found in qpl check")
+        return False
+    if (round(np.real(vector[0]),4)
+        == -1.0*round(np.real(vector[2]),4)):
+        pass
+    else:
+        print("|000> and |010> are not oppositely-signed amplitudes\n"+
+              "as expected. Pass to API aborted.\nPlease check your gates"
+              +"\nas an undue property was found in qpl check")
+        return False
+    if (round(np.real(vector[4]),4) == round(np.real(vector[6]),4) and
+        round(np.imag(vector[1]),4) == round(np.imag(vector[3]),4) and
+        abs(round(np.real(vector[4]),2)) > 0.1 and
+        abs(round(np.imag(vector[1]),2)) > 0.1 and
+        abs(round(np.real(vector[4]),2)) < 0.5 and
+        abs(round(np.real(vector[4]),2)) < 0.5
+    ):
+        pass
+    else:
+        print("Failed to meet any of the following conditions:-\n" +
+              "|001> and |011> are imaginary, equal and less than the "+
+              "magnitude of the RX(ZZ) evolution. \n|100> and |110> are "+
+              "real, equal and less than the magnitude of the RX(ZZ)"+
+              "evolution\n-as expected. "+
+              "Pass to API aborted.\nPlease check your gates"
+              +"\nas an undue property was found in qpl check")
+        return False
+    squared = np.round(squared,3)
+    if np.all(squared==squared[0]):
+        return True
+    else:
+        print("Probabilities not uniform. Please review gate set\n"+
+              "API call aborted for gate revision")
+        return False
+
+def check_ghz(counts):
+    """
+    Test function, all GHZ states satisfy expected properties. In this case,
+    check only 000 and 111 occured from the simulator
+
+    Args:
+    counts:list[str] = data.meas.get_bitstrings() return to be processed.
+    """
+    num = range(len(counts))
+    series = pd.Series(counts,index=num)
+    series = series.astype(str)
+    unexpecteds = series[(series!='000') & (series!='111')]
+    if unexpecteds.size==0:
+        return True
+    else:
+        print("Pass to API aborted.\nPlease check your gates"
+              +"\nas an undue property was found in AER check"
+        )
+        return False
+
+def AER_count_check(circuit, backend, shots=1024):
+    """
+    Strictly to visualize the outcome before proceding.
+
+    Args:
+        circuit (QuantumCircuit): The quantum circuit to run.
+        backend: The Qiskit backend (real device or simulator).
+        shots (int): The number of shots to run the circuit.
+
+    Returns:
+        dict = A count of each qubit combination across shots.
+    """
+    pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+    isa_circuit = pm.run(circuit)
+    sampler = Sampler(mode=backend)
+    job = sampler.run([isa_circuit], shots=shots)
+    result = job.result()
+    return result[0].data.meas.get_counts()
+
 def preset_run_collect(circuit, backend, shots=1024):
     """
     Runs a quantum circuit on a specified backend and returns the measurement counts.
@@ -64,26 +189,6 @@ def preset_run_collect(circuit, backend, shots=1024):
     result = job.result()
 
     return result[0].data.meas.get_bitstrings()
-
-def check_ghz(counts):
-    """
-    Test function, all GHZ states satisfy expected properties. In this case,
-    check only 000 and 111 occured from the simulator
-
-    Args:
-    counts:list[str] = data.meas.get_bitstrings() return to be processed.
-    """
-    num = range(len(counts))
-    series = pd.Series(counts,index=num)
-    series = series.astype(str)
-    unexpecteds = series[(series!='000') & (series!='111')]
-    if unexpecteds.size==0:
-        return True
-    else:
-        print("Pass to API aborted.\nPlease check your gates"
-              +"\nas an undue property was found in AER check"
-        )
-        return False
 
 def make_csv(counts,experiment_string,trial_string):
     """
@@ -119,6 +224,17 @@ ghz.cx(0, 1)
 ghz.cx(0, 2)
 ghz.measure_all()
 
+sechos = QuantumCircuit(3)
+sechos.rx(np.pi*0.5,0)
+sechos.rzz(np.pi*0.5,0,1)
+sechos.ryy(np.pi*-0.5,0,1)
+sechos.ry(np.pi*2.0,0)
+sechos.rzz(np.pi*0.5,1,2)
+sechos.ryy(np.pi*-0.5,1,2)
+sechos.rxx(np.pi,0,1)
+sechos.rxx(np.pi,1,2)
+sechos.measure_all()
+
 service = QiskitRuntimeService(channel="ibm_quantum_platform")
 service = QiskitRuntimeService()
 backend = AerSimulator()
@@ -128,6 +244,14 @@ if good_go==True:
     expt_str = 'GHZ'
     trial_str = '1'
     backend = service.least_busy(operational=True, simulator=False, min_num_qubits=3)
-    backend = AerSimulator()
+    backend = AerSimulator() #trip fuse against run-all
     result = preset_run_collect(ghz, backend, shots=1024)
+    make_csv(result,expt_str,trial_str)
+good_go = check_sechos_expt()
+if good_go==True:
+    expt_str = 'fabric_spin_echo'
+    trial_str = '1'
+    backend = service.least_busy(operational=True, simulator=False, min_num_qubits=3)
+    backend = AerSimulator() #trip fuse against run-all
+    result = preset_run_collect(sechos, backend, shots=1024)
     make_csv(result,expt_str,trial_str)
